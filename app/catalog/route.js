0430d2d9ed408c5cbb7736cd787f3e089bda2435
jakefrey <jake.frey94@gmail.com>
2020-05-26 00:11:41 -0700

    First release done!

--- app/catalog/route.js
+++ app/catalog/route.js
@@ -1,10 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-
-    setupController: function(controller) {
-        this._super(...arguments);
-
-        window.scrollTo(0, 0);
-    },
 });
